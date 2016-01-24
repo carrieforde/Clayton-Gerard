e4fffd7ddcceedfe2246ae13ccafbdb6f262cc0f
Carrie Forde <carrie@carrieforde.com>
2016-01-24 15:45:40 -0800

    Add some helper JS to improve styling

--- js/alcatraz-clayton-theme.js
+++ js/alcatraz-clayton-theme.js
@@ -4,6 +4,19 @@
 
 ( function( $ ) {
 
+	// Executes when the document is ready
+	jQuery( document ).ready( function( $ ) {
 
+		// Add classes to paragraphs before headings
+		$( 'h1, h2, h3, h4, h5, h6' ).prev( 'p' ).addClass( 'extra-p-margin' );
 
-} )( jQuery );
+		// Remove empty p tags
+		$( 'p' ).each(function() {
+			var $this = $( this );
+			if( $this.html().replace(/\s|&nbsp;/g, '' ).length === 0 )
+				$this.remove();
+		});
+
+	});
+
+})( jQuery );
