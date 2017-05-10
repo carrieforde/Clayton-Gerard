4abc891ce10bbb37dc27b37f95e7e961e6c74afc
Carrie Forde <carrie@carrieforde.com>
2017-05-09 17:31:51 -0700

    Add revised markup and styles for footer

---
+++ template-parts/globals/global-site-footer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Clayton Gerard Footer
+ *
+ * @package Clayton Gerard
+ */
+?>
+
+<footer id="site-footer" class="site-footer" role="contentinfo">
+
+	<div class="footer-wrap">
+
+		<?php do_action( 'alcatraz_footer' ); ?>
+
+	</div>
+
+</footer>
