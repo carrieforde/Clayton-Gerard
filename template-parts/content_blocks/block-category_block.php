fe40967a0b624c773a87566935b8eb1259357664
Carrie Forde <carrie@carrieforde.com>
2017-05-31 18:35:27 -0700

    Adds markup for category block

--- template-parts/content_blocks/block-category_block.php
+++ template-parts/content_blocks/block-category_block.php
@@ -14,7 +14,33 @@ if ( ! $post_id ) {
 $prefix = ( ! empty( $component ) ) ? 'blocks_' . $count . '_' : '';
 
 // This block's fields.
+$heading    = get_post_meta( $post_id, $prefix . 'heading', true );
+$categories = get_post_meta( $post_id, $prefix . 'categories', true );
+$i          = 0; // iterator for our loop.
 
 ?>
 
-<section class="category-block"></section>
+<section class="category-block">
+
+	<div class="row">
+
+		<ul class="categories">
+			<?php foreach ( $categories as $category ) :
+
+				$link = get_category_link( $category );
+				$name = get_the_category_by_ID( $category ); ?>
+
+				<li class="category">
+					<a href="<?php echo esc_url( $link ); ?>" class="button"><?php echo esc_html( $name ); ?></a>
+				</li>
+
+				
+				<?php $i++;
+
+				if ( 6 === $i ) {
+					break;
+				} ?>
+			<?php endforeach; ?>
+		</ul>
+	</div>
+</section>
