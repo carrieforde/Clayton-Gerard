1fd42d73a71c360afd9b2e4c5d4b322330432197
Carrie Forde <carrie@carrieforde.com>
2017-05-31 18:25:14 -0700

    Adds markup to content blog with button block

--- template-parts/content_blocks/block-content_block_with_button.php
+++ template-parts/content_blocks/block-content_block_with_button.php
@@ -11,8 +11,23 @@ if ( ! $post_id ) {
 }
 
 // If this is used within flexible contnet, we need to component prefix.
-$prefix    = ( ! empty( $component ) ) ? 'blocks_' . $count . '_' : '';
+$prefix = ( ! empty( $component ) ) ? 'blocks_' . $count . '_' : '';
+
+// This block's fields.
+$heading     = get_post_meta( $post_id, $prefix . 'heading', true );
+$text        = get_post_meta( $post_id, $prefix . 'text', true );
+$button_text = get_post_meta( $post_id, $prefix . 'button_text', true );
+$button_link = get_post_meta( $post_id, $prefix . 'button_link', true );
 
 ?>
 
-<section class="content-block-with-button"></section>
+<section class="content-block-with-button">
+
+	<div class="row">
+		<h2 class="heading"><?php echo esc_html( $heading ); ?></h2>
+		<div class="content">
+			<?php echo wp_kses_post( wpautop( $text ) ); ?>
+		</div>
+		<a href="<?php echo esc_url( $button_link ); ?>" class="button"><?php echo esc_html( $button_text ); ?></a>
+	</div><!-- .row -->
+</section>
