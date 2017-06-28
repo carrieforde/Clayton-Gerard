2b0482929a78c0a58655d75470afae013c5b1423
Carrie Forde <carrie@carrieforde.com>
2017-06-27 18:32:34 -0700

    Adds styles for latest projects module

--- template-parts/content_blocks/block-latest_projects.php
+++ template-parts/content_blocks/block-latest_projects.php
@@ -14,17 +14,17 @@ if ( ! $post_id ) {
 $prefix = ( ! empty( $component ) ) ? 'blocks_' . $count . '_' : '';
 
 // This block's fields.
-$heading = get_post_meta( $post_id, $prefix . 'heading', true );
+$heading         = get_post_meta( $post_id, $prefix . 'heading', true );
 $number_of_posts = get_post_meta( $post_id, $prefix . 'number_of_posts', true ); ?>
 
-<section class="latest-posts full-width container">
+<section class="latest-projects full-width container">
 	<div class="row">
 		<h2 class="heading"><?php echo esc_html( $heading ); ?></h2>
 		<?php cf_cg_fetch_posts( array(
-			'post_type' => 'cg-portfolio',
+			'post_type'      => 'cg-portfolio',
 			'posts_per_page' => $number_of_posts,
 			'template_part'  => 'template-parts/content-latest-project',
 		) ); ?>
-		<a href="<?php echo esc_url( cf_cg_get_blog_posts_page_url() ); ?>" class="button"><?php esc_html_e( 'More Posts', 'claytongerard' ); ?></a>
+		<footer class="footer"><a href="<?php echo esc_url( cf_cg_get_blog_posts_page_url() ); ?>" class="button"><?php esc_html_e( 'More Posts', 'claytongerard' ); ?></a></footer>
 	</div>
 </section>
